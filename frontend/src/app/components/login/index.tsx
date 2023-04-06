9af1cd54265fe6539aa8d89bad172ca38f29d61d
danielrosenosi <devroseno@gmail.com>
2023-04-06 17:01:24 -0300

    feat: small adjustments

--- frontend/src/app/components/login/index.tsx
+++ frontend/src/app/components/login/index.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { FiLogIn } from 'react-icons/fi';
 
 export function Login() {
     const [email, setEmail] = useState("");
@@ -12,8 +15,25 @@ export function Login() {
         <div className="login-container">
             <section className="form">
                 <form onSubmit={handleSubmit}>
-                    <input>
-                    </input>
+                    <input
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+
+                    <input
+                        placeholder="Senha"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#3498db" />
+                        Não tenho cadastro
+                    </Link>
                 </form>
             </section>
         </div>
